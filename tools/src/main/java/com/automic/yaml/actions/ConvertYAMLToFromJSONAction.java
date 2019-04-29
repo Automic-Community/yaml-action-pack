210ba3db8fec05e92e4d200dbd9289648d8d9359
Priyadarshni Tripathi <priyadarshni.tripathi@nagarro.com>
2019-04-29 12:49:56 +0530

    Base code in Sync with Remove YAML

---
+++ tools/src/main/java/com/automic/yaml/actions/ConvertYAMLToFromJSONAction.java
@@ -0,0 +1,14 @@
+package com.automic.yaml.actions;
+
+import com.automic.yaml.base.AbstractYAMLAction;
+import com.automic.yaml.exception.AutomicException;
+
+public class ConvertYAMLToFromJSONAction extends AbstractYAMLAction {
+
+	@Override
+	protected void executeSpecific() throws AutomicException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ tools/src/main/java/com/automic/yaml/actions/RemoveYAMLAction.java
@@ -0,0 +1,82 @@
+package com.automic.yaml.actions;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.automic.yaml.base.AbstractYAMLAction;
+import com.automic.yaml.constants.Constants;
+import com.automic.yaml.constants.ExceptionConstants;
+import com.automic.yaml.exception.AutomicException;
+import com.automic.yaml.util.CommonUtil;
+import com.automic.yaml.util.ConsoleWriter;
+import com.automic.yaml.util.YamlOperations;
+import com.automic.yaml.util.YamlUtils;
+
+/**
+ * 
+ * @author priyadarshnitripathi
+ *
+ */
+public class RemoveYAMLAction extends AbstractYAMLAction {
+	
+	private String yamlElementPath;
+	private boolean isFail=true;
+	private String yamlDownloadPath;
+	private String output;
+
+	private YamlOperations yamlOperations = new YamlOperations();
+
+	/**
+	 * Initializes a newly created {@code SendMessageAction}
+	 */
+	public RemoveYAMLAction() {
+		addOption(Constants.YAML_ELEMENT_PATH, true, "Path to Yaml Element");
+		addOption(Constants.IS_FAIL, false, "Fail if path does not exist");
+		addOption(Constants.YAML_DOWNLOAD_PATH, false, "Download Path");
+
+	}
+	
+	@Override
+	protected void executeSpecific() throws AutomicException {
+		prepareAndValidateInputs();
+		try {
+
+			output = yamlOperations.remove(content, yamlElementPath, yamlDownloadPath, isFail);
+			if(output==null)
+				output=content;
+			if (CommonUtil.checkNotEmpty(yamlDownloadPath)) {
+				YamlUtils.writeContentToFile(yamlDownloadPath, output.toString());
+			}
+			
+			ConsoleWriter.writeln("UC4RB_FILE_CONTENT::=========="+"\n" + output.toString());
+			ConsoleWriter.writeln("========================");
+		} catch (Exception exception) {
+			ConsoleWriter.writeln(exception);
+			throw new AutomicException(exception.getMessage());
+		}
+	}
+
+	private void prepareAndValidateInputs() throws AutomicException {
+		yamlElementPath = getOptionValue(Constants.YAML_ELEMENT_PATH);
+		if (StringUtils.isEmpty(yamlElementPath)) {
+			throw new AutomicException(ExceptionConstants.UPDATE_YAML_PATH_EMPTY_MSG);
+		}
+		
+		isFail=CommonUtil.convert2Bool(getOptionValue(Constants.IS_FAIL));
+		yamlDownloadPath=getOptionValue(Constants.YAML_DOWNLOAD_PATH);
+		if (!StringUtils.isEmpty(yamlDownloadPath)) {
+			try {
+
+				File file = new File(yamlDownloadPath);
+				file.createNewFile();
+			} catch (IOException e) {
+				ConsoleWriter.writeln(e);
+				throw new AutomicException(String.format(ExceptionConstants.UNABLE_TO_CREATE_FILE, yamlDownloadPath));
+			}
+		}
+	}
+
+	
+}
