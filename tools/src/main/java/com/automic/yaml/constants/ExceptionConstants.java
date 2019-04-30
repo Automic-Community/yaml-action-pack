c9bc4a78250c8c9b9f2329fc15f86276128f32f0
Savita <savita@nagarro.com>
2019-04-30 12:29:54 +0530

    PMARA-11867:Java Base Code

--- tools/src/main/java/com/automic/yaml/constants/ExceptionConstants.java
+++ tools/src/main/java/com/automic/yaml/constants/ExceptionConstants.java
@@ -22,6 +22,10 @@ public class ExceptionConstants {
 	public static final String UNABLE_UPDATE_CONTENT_MSG = "Unable to update YAML Content.\n";
 	public static final String INVALID_FORMAT_FOR_CONVERSION = "Invalid Format for Conversion, either provide JSON or YAML";
 	public static final String YAML_PATH_EMPTY_MSG = "Path to YAML element is empty.";
+	public static final String UPDATE_YAML_PATH_EMPTY_MSG = "Path to YAML emlement is empty.";
+	public static final String YAML_TARGET_POSITION_EMPTY_MSG = "Target location to add new element is empty.";
+	public static final String YAML_KEY_EMPTY_MSG = "Provided Key is empty.";
+	public static final String YAML_VALUE_EMPTY_MSG = "Provided Value is empty.";
 	public static final String UNABLE_TO_CREATE_FILE = "Error in creating file at Download Path : [%s].";
 	
 	private ExceptionConstants() {
